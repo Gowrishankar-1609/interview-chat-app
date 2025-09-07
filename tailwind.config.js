272244a62b06a011f7169f38704dae7ba3402cc0
Gowrishankar-1609 <22501a4411@pvpsit.ac.in>
2025-09-07 10:57:06 +0530

    Fix Tailwind CSS styles not applying

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
